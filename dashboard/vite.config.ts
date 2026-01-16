f0e6aa0f50fc59ad324d6103aa4bd16ecd4de4f1
Syril Jacob <syrilj@users.noreply.github.com>
2026-01-15 17:11:43 -0700

    msc: updates to ci/cd

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:51052',
+        target: 'http://localhost:51051',
         changeOrigin: true,
       }
     }
